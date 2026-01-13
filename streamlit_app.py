d97901716f8e614d34ad27cdecd67fde0ba2a82c
anirudh_vijayabalaji <158029183+AnirudhVijayabalaji@users.noreply.github.com>
2026-01-13 19:16:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,24 +9,24 @@ st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
   """ Choose fruits you want to in your Custom Smoothie!
   """)
+cnx=st.connection("snowflake",type="snowflake")
+session=cnx.session()
 name_on_order= st.text_input('Name on the smoothie:') 
 st.write('The name on the smoothie will be:', name_on_order)
 
-cnx=st.connection("snowflake",type="snowflake")
-session=cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list= st.multiselect('choose up to 5 ingredients:',my_dataframe,max_selections=5)
 
 if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
          ingredients_string+=fruit_chosen + ' '
-    #st.write( ingredients_string)
+    st.write( ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) 
     values ('""" + ingredients_string + """','"""+ name_on_order + """')""" 
-    #st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
 
     
     time_to_insert= st.button('Submit Order')
