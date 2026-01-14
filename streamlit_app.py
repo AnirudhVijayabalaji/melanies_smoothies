9d8ba01c3dae55e6833e4fbd6b0326f3f8be60af
anirudh_vijayabalaji <158029183+AnirudhVijayabalaji@users.noreply.github.com>
2026-01-14 09:32:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,13 +22,15 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
          ingredients_string+=fruit_chosen + ' '
     st.write( ingredients_string)
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
+
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) 
     values ('""" + ingredients_string + """','"""+ name_on_order + """')""" 
     st.write(my_insert_stmt)
 
-   smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-   st.text(smoothiefroot_response)
+    
 
 
     
